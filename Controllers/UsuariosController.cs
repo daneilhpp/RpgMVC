50729077118f2ac45af8d4344b0320f2613ef2ca
daneilhpp <carvalhosilvad88@gmail.com>
2022-05-19 22:47:38 -0300

    [Commit01] Initial Commit, Login and Register Screen

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgMVC.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+
+namespace RpgMVC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public string uriBase = "http://localhost:5000/Usuarios/";
+
+            
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("CadastrarUsuario");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+               HttpClient httpClient = new HttpClient();
+               string uriComplementar = "Registrar";
+
+               var content = new StringContent(JsonConvert.SerializeObject(u));
+               content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+               HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+               string serialized = await response.Content.ReadAsStringAsync();
+
+               if(response.StatusCode == System.Net.HttpStatusCode.OK)
+               {
+                   TempData["Mensagem"] = string.Format("Usuário {0} Registrado com sucesso! Faça login para acessar.",u.Username);
+
+                   return View("AutenticarUsuario");
+               }
+               else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Models/UsuarioViewModel.cs
@@ -0,0 +1,10 @@
+namespace RpgMVC.Models
+{
+    public class UsuarioViewModel
+    {
+        public int Id{get;set;}
+        public string Username{get;set;}
+        public string PasswordString{get;set;}
+        public byte[] Foto{get;set;}
+    }
+}

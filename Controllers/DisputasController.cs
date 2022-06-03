39f216536b29d8edaf931de084dab30907813999
daneilhpp <carvalhosilvad88@gmail.com>
2022-06-03 14:03:01 -0300

    [Commit05] Disputas addition

---
+++ Controllers/DisputasController.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using RpgMVC.Models;
+
+namespace RpgMVC.Controllers
+{
+    public class DisputasController : Controller
+    {
+        public string uriBase = "http://localhost:5000/Disputas/";
+
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",token);
+
+                string uriBuscaPersonagens = "http://localhost:5000/Personagens/GetAll";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuscaPersonagens);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<PersonagemViewModel> listaPersonagens = await Task.Run(() => JsonConvert.DeserializeObject<List<PersonagemViewModel>>(serialized));
+
+                    ViewBag.ListaAtacantes = listaPersonagens;
+                    ViewBag.ListaOponentes = listaPersonagens;
+
+                    return View();
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch(System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> IndexAsync(DisputaViewModel disputa)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Arma";
+
+                var content = new StringContent(JsonConvert.SerializeObject(disputa));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    disputa = await Task.Run(() => JsonConvert.DeserializeObject<DisputaViewModel>(serialized));
+                    TempData["Mensagem"] = disputa.Narracao;
+
+                    return RedirectToAction("Index","Personagem");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch(System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        //Continuar do item 7
+    }
+}
